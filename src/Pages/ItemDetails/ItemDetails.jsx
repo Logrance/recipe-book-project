bfb027c1f5e14697081488162fa547209c6d6b4e
chyVacheck <dmytro.shah.work@gmail.com>
2024-02-08 15:27:26 +0100

    feat: make page NotFound more universal
    - show a message when a recipe is not found

--- src/Pages/ItemDetails/ItemDetails.jsx
+++ src/Pages/ItemDetails/ItemDetails.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 /* eslint-disable react/prop-types */
 
 import ItemCard from './../../components/ItemCard/ItemCard';
+import NotFound from '../NotFound/NotFound';
 
 export default function ItemDetails({ allRecipes, deleteRecipe }) {
   const { recipeId } = useParams();
@@ -12,11 +13,13 @@ export default function ItemDetails({ allRecipes, deleteRecipe }) {
     return element.id == recipeId;
   });
 
-  return (
+  return recipeDetails ? (
     <ItemCard
       recipe={recipeDetails}
       handleDelete={deleteRecipe}
       isNeedDisplayIno={true}
     />
+  ) : (
+    <NotFound>We have no this recipe</NotFound>
   );
 }
--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,16 +1,17 @@
+/* eslint-disable react/prop-types */
 // ! modules
 import { NavLink } from 'react-router-dom';
 
 // styles
 import './NotFound.css';
 
-
-export default function NotFound() {
+export default function NotFound({ children = 'Page not Found' }) {
   return (
     <article className='not-found'>
-      <h1 className='text text_size_big text_color_dark' >Not found page</h1>
-      <NavLink className='link' to={'/'} >Go to main</NavLink>
+      <h1 className='text text_size_big text_color_dark'>{children}</h1>
+      <NavLink className='link text' to='/'>
+        Go to main
+      </NavLink>
     </article>
-
   );
 }
